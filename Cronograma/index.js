cf0e4c7a1cb8720598e4e42879423a62f31a6c06
anabenetti <79387054+aluy-ab@users.noreply.github.com>
2024-11-27 14:49:01 -0300

    Create index.js

---
+++ Cronograma/index.js
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Aguillar Family Wine Festival</title>
+  <link rel="stylesheet" type="text/css" href="reset.css" />
+  <link rel="stylesheet" type="text/css" href="style.css" />
+  <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+</head>
+
+<body>
+  <header>
+    <h1>Festa de Final de Ano da Família</h1>
+  </header>
+  
+  <div class="container">
+    <table>
+      <thead>
+        <tr>
+          <th colspan="2">
+            <h1>Cronograma</h1>
+          </th>
+        </tr>
+        <tr>
+          <th>
+            <h2>Horário</h2>
+          </th>
+          <th>
+            <h2>Atividade</h2>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="left">
+            <h3>12:00</h3>
+          </td>
+          <td>
+            <h3>Chegada e comidas ao forno</h3>
+          </td>
+        </tr>
+        <tr>
+          <td class="left">
+            <h3>13:00</h3>
+          </td>
+          <td>
+            <h3>Contagem de histórias</h3>
+          </td>
+        </tr>
+       <tr>
+          <td class="left">
+            <h3>14:00</h3>
+          </td>
+          <td>
+            <h3>Degustação do almoço</h3>
+          </td>
+        </tr>
+        <tr>
+          <td class="left">
+            <h3>15:00</h3>
+          </td>
+          <td>
+            <h3>Apreciação às sobremesas</h3>
+          </td>
+        </tr>
+        <tr>
+          <td class="left">
+            <h3>16:00</h3>
+          </td>
+          <td>
+            <h3>Despedidas</h3>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  
+  <footer>
+    <h3>Contato</h3>
+    <h3>Localização</h3>
+    <h3>Política de Privacidade</h3>
+  </footer>
+</body>
+
+</html>
